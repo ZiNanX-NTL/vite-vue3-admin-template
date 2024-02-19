a911f7e31cc810f3b9e8de5da3e79e1f93fbc0ba
ZiNanX_NTL <702668400@qq.com>
2024-02-19 11:11:08 +0800

    修复顶部菜单模式横向菜单定位失败问题

--- src/layouts/common/global-header/components/HeaderMenu.vue
+++ src/layouts/common/global-header/components/HeaderMenu.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="flex-1-hidden h-full px-10px">
     <n-scrollbar :x-scrollable="true" class="flex-1-hidden h-full" content-class="h-full">
-      <div class="flex-y-center h-full" :style="{ justifyContent: theme.headerMenu.horizontalPosition }">
+      <div class="flex-y-center h-full">
         <n-menu
           :value="activeKey"
           mode="horizontal"
+          responsive
           :options="routeStore.menus"
           :inverted="theme.header.inverted"
           @update:value="handleUpdateMenu"
@@ -36,5 +37,8 @@ function handleUpdateMenu(_key, item) {
 <style scoped>
 :deep(.n-menu-item-content-header) {
   overflow: inherit !important;
+}
+:deep(.n-menu > .v-overflow) {
+  justify-content: v-bind('theme.headerMenu.horizontalPosition');
 }
 </style>
