4be83f58ae2572aedfe9d2212d6f0a5c986114f3
ZiNanX_NTL <702668400@qq.com>
2024-08-14 17:51:59 +0800

    feat(projects): 新增选座图坐标尺

--- src/views/test-page/seat2/index.vue
+++ src/views/test-page/seat2/index.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script setup lang="ts">
-import { App, Group, Image, PointerEvent } from 'leafer-ui';
+import { App, Group, Image, Rect, Box, PointerEvent, ZoomEvent, MoveEvent } from 'leafer-ui';
 import '@leafer-in/editor';
 import '@leafer-in/view';
 import '@leafer-in/state';
@@ -25,10 +25,27 @@ function init() {
     view: 'leafer-view',
     editor: {
       selector: false
-    }
+    },
+    ground: { type: 'custom' },
+    move: { drag: 'auto', dragAnimate: true }
   });
+  console.log(app.ground.width);
+  app.ground.zIndex = 100;
   scroll = new ScrollBar(app, { theme: themeStore.darkMode ? 'dark' : 'light' });
 
+  // 座位大小
+  const seatSize = 30;
+  // 座位间隔
+  const seatGap = 10;
+  // 刻度尺宽
+  const rulerItemWidth = 15;
+  // 刻度尺高
+  const rulerItemHeight = 15;
+  /**
+   * 座位布局
+   * 0 表示不可用
+   * 1 表示可用
+   */
   const seatLayout = [
     [1, 0, 1, 1, 0, 1, 1, 1, 1],
     [0, 0, 1, 1, 1, 1, 1, 0, 0],
@@ -75,13 +92,73 @@ function init() {
   );
   // console.log(renderMap);
 
+  // 绘制编号标尺
+  function drawRuler() {
+    const rulerBg: Rect[] = [];
+    const xRulerList: Rect[] = [];
+    const yRulerList: Rect[] = [];
+    let startY = (seatSize - rulerItemHeight) / 2;
+    let startX = (seatSize - rulerItemWidth) / 2;
+    seatLayout.forEach((row, rowIndex) => {
+      if (rowIndex === 0) {
+        row.forEach((_col, colIndex) => {
+          const colRuler = createRuler(startX, 0, String(colIndex + 1));
+          xRulerList.push(colRuler);
+          startX += seatSize + seatGap;
+        });
+        const colRulerBg = createRulerBg('xBg', app.ground.width, rulerItemHeight);
+        rulerBg.push(colRulerBg);
+      }
+      const rowRuler = createRuler(0, startY, String(rowIndex + 1));
+      yRulerList.push(rowRuler);
+      startY += seatSize + seatGap;
+    });
+    const rowRulerBg = createRulerBg('yBg', rulerItemWidth, app.ground.height);
+    rulerBg.push(rowRulerBg);
+
+    return [rulerBg, xRulerList, yRulerList];
+  }
+
+  /** 创建标尺刻度项 */
+  function createRuler(x?: number, y?: number, text?: string) {
+    const ruler = new Box({
+      x,
+      y,
+      fill: 'rgb(50,205,121)',
+      cornerRadius: 5,
+      width: rulerItemWidth,
+      height: rulerItemHeight,
+      children: [
+        {
+          x: rulerItemWidth / 2,
+          y: rulerItemHeight / 2,
+          tag: 'Text',
+          text,
+          fontSize: 10,
+          fill: 'black',
+          textAlign: 'center',
+          verticalAlign: 'middle'
+        }
+      ]
+    });
+    return ruler;
+  }
+  /** 绘制标尺背景 */
+  function createRulerBg(id?: string, width?: number, height?: number) {
+    const rulerBg = new Rect({
+      id,
+      x: 0,
+      y: 0,
+      width,
+      height,
+      fill: '#fff'
+    });
+    return rulerBg;
+  }
+
   // 绘制座位
   function drawSeats(drawMap: typeof renderMap) {
     const seatList: Image[] = [];
-    // 座位大小
-    const seatSize = 30;
-    // 座位间隔
-    const seatGap = 10;
 
     let currentY = 0;
     drawMap.forEach(row => {
@@ -119,10 +196,24 @@ function init() {
     return image;
   }
 
-  const seatList = drawSeats(renderMap);
+  // 绘制标尺
+  const [rulerBg, xRulerList, yRulerList] = drawRuler();
+  const rulerBgGroup = new Group({
+    children: rulerBg
+  });
+  const xRulerGroup = new Group({
+    children: xRulerList
+  });
+  const yRulerGroup = new Group({
+    children: yRulerList
+  });
+  app.ground.add(rulerBgGroup);
+  app.ground.add(xRulerGroup);
+  app.ground.add(yRulerGroup);
 
+  // 绘制座位
+  const seatList = drawSeats(renderMap);
   const group = new Group({
-    draggable: true,
     children: seatList
   });
   app.tree.add(group);
@@ -146,9 +237,42 @@ function init() {
     console.log(seatData);
   });
 
+  // 自定义ground移动缩放操作
+  app.ground.on(ZoomEvent.BEFORE_ZOOM, (e: ZoomEvent) => {
+    const xBg = rulerBgGroup.findId('xBg');
+    const yBg = rulerBgGroup.findId('yBg');
+    if (xBg && yBg) {
+      xBg.height! *= e.scale;
+      yBg.width! *= e.scale;
+    }
+    const centerX = { x: e.x, y: 0 };
+    xRulerGroup.scaleOfWorld(centerX, e.scale);
+    const centerY = { x: 0, y: e.y };
+    yRulerGroup.scaleOfWorld(centerY, e.scale);
+  });
+  app.ground.on(MoveEvent.BEFORE_MOVE, (e: MoveEvent) => {
+    xRulerGroup.moveWorld(e.moveX, 0);
+    yRulerGroup.moveWorld(0, e.moveY);
+  });
+
+  // 更新ruler位置
+  function updateRulerPosition() {
+    const groupBoundsData = group.getBounds();
+    rulerBgGroup.scale = app.tree.scale!;
+    xRulerGroup.x = groupBoundsData.x;
+    yRulerGroup.y = groupBoundsData.y;
+    xRulerGroup.scale = app.tree.scale!;
+    yRulerGroup.scale = app.tree.scale!;
+  }
+
   setTimeout(() => {
     app.tree.zoom(group, 0, true);
+    updateRulerPosition();
   });
+  setTimeout(() => {
+    app.tree.zoom('in');
+    updateRulerPosition();
+  }, 3000);
 }
 
 watch(
