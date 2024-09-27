b2df0f16f0dd9917d4c2944ad9bcda7b92e60fc7
ZiNanX_NTL <702668400@qq.com>
2024-09-27 16:03:23 +0800

    fix(hooks): 修复useTable,transformer方法返回data默认值

--- src/hooks/business/useTable.js
+++ src/hooks/business/useTable.js
@@ -75,7 +75,12 @@ export function useTable(config) {
 
     const response = await apiFn(requestParams);
 
-    const { data: tableData, pageNum = pagination.page, pageSize = pagination.pageSize, total } = transformer(response);
+    const {
+      data: tableData = [],
+      pageNum = pagination.page,
+      pageSize = pagination.pageSize,
+      total
+    } = transformer(response);
 
     data.value = tableData;
 
