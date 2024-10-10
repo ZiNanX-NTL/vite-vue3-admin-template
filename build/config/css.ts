6eb006d9ab8a3ba7e09ef0c13a58081f31137f63
ZiNanX_NTL <702668400@qq.com>
2024-10-10 13:55:48 +0800

    chore(projects): 更新sass config

--- build/config/css.ts
+++ build/config/css.ts
@@ -3,6 +3,7 @@ import type { CSSOptions } from 'vite';
 export const viteCss: CSSOptions = {
   preprocessorOptions: {
     scss: {
+      api: 'modern-compiler',
       additionalData: `@use "@/styles/scss/global.scss" as *;`
     }
   }
