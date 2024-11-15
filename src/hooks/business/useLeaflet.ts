ed3b48244bf6bb849dc8873f9a9526313ff54b3d
ZiNanX_NTL <702668400@qq.com>
2024-11-15 17:58:58 +0800

    refactor(hooks): 重构useLeaflet

--- src/hooks/business/useLeaflet.ts
+++ src/hooks/business/useLeaflet.ts
@@ -1,9 +1,11 @@
-import { ref, nextTick, watch, onScopeDispose, effectScope } from 'vue';
-import { useScriptTag, useElementSize } from '@vueuse/core';
+import { ref } from 'vue';
+import { useScriptTag } from '@vueuse/core';
 import L from 'leaflet';
 import '@/plugins/leaflet/leaflet-marker.js';
 import 'leaflet-easybutton';
 import { ESRI_LEAFLET_CDN } from '@/config';
+import { router } from '@/router';
+import { useRender } from '../common';
 
 interface MapHooks {
   onRender?: (map: L.Map) => void | Promise<void>;
@@ -12,31 +14,50 @@ interface MapHooks {
 }
 
 export function useLeaflet(options: L.MapOptions = {}, hooks: MapHooks = {}) {
-  const scope = effectScope();
-
   const { load: loadEsriLeaflet } = useScriptTag(ESRI_LEAFLET_CDN);
 
   const domRef = ref<HTMLElement | null>(null);
-  const initialSize = { width: 0, height: 0 };
-  const { width, height } = useElementSize(domRef, initialSize);
 
-  let map: L.Map | null = null;
+  const route = unref(router.currentRoute);
+  const isKeepAlive = route.meta.keepAlive;
 
   const { onRender, onUpdated, onDestroy } = hooks;
 
-  /**
-   * whether can render map
-   *
-   * when domRef is ready and initialSize is valid
-   */
-  function canRender() {
-    return domRef.value && initialSize.width > 0 && initialSize.height > 0;
-  }
+  const { instance: mapInstance, isRendered } = useRender(domRef, {
+    async render() {
+      await loadEsriLeaflet(true);
 
-  /** is map rendered */
-  function isRendered() {
-    return Boolean(domRef.value && map);
-  }
+      const southWest = L.latLng(-90, -180);
+      const northEast = L.latLng(90, 180);
+      const bounds = L.latLngBounds(southWest, northEast);
+
+      const map = L.map(domRef.value as HTMLElement, {
+        crs: L.CRS.EPSG4326,
+        maxBounds: bounds,
+        minZoom: 1,
+        attributionControl: false,
+        zoomControl: false,
+        ...options
+      }).setView([48.19, 131.55], 6);
+      await onRender?.(map);
+
+      return map;
+    },
+    resize(instance) {
+      instance?.value?.invalidateSize();
+    },
+    async destroy(instance, isForce = false) {
+      if (!instance?.value) return;
+
+      await onDestroy?.(instance?.value);
+      if (!isKeepAlive || isForce) {
+        instance?.value?.off();
+        instance?.value?.remove();
+        if (!instance.value) return;
+        instance.value = null;
+      }
+    }
+  });
 
   /**
    * update map
@@ -46,87 +67,10 @@ export function useLeaflet(options: L.MapOptions = {}, hooks: MapHooks = {}) {
   async function updateMap(callback: (map: L.Map) => void = () => {}) {
     if (!isRendered()) return;
 
-    callback(map!);
+    callback(mapInstance.value!);
 
-    await onUpdated?.(map!);
+    await onUpdated?.(mapInstance.value!);
   }
-
-  /** render map */
-  async function render() {
-    if (!isRendered()) {
-      await nextTick();
-      await loadEsriLeaflet(true);
-
-      const southWest = L.latLng(-90, -180);
-      const northEast = L.latLng(90, 180);
-      const bounds = L.latLngBounds(southWest, northEast);
-
-      // 初始化
-      if (!map) {
-        map = L.map(domRef.value as HTMLElement, {
-          crs: L.CRS.EPSG4326,
-          maxBounds: bounds,
-          minZoom: 1,
-          attributionControl: false,
-          zoomControl: false,
-          ...options
-        }).setView([48.19, 131.55], 6);
-        await onRender?.(map);
-      }
-    }
-  }
-
-  /** resize map */
-  function resize() {
-    map!.invalidateSize();
-  }
-
-  /** destroy map */
-  async function destroy() {
-    if (!map) return;
-    await onDestroy?.(map);
-    map.off();
-    map.remove();
-    if (!map) return;
-    map = null;
-  }
-
-  /**
-   * render map by size
-   *
-   * @param w width
-   * @param h height
-   */
-  async function renderMapBySize(w: number, h: number) {
-    initialSize.width = w;
-    initialSize.height = h;
-
-    // size is abnormal, destroy map
-    if (!canRender()) {
-      await destroy();
-
-      return;
-    }
-
-    // resize map
-    if (isRendered()) {
-      resize();
-    }
-
-    // render map
-    await render();
-  }
-
-  scope.run(() => {
-    watch([width, height], ([newWidth, newHeight]) => {
-      renderMapBySize(newWidth, newHeight);
-    });
-  });
-
-  onScopeDispose(() => {
-    destroy();
-    scope.stop();
-  });
 
   return {
     domRef,
