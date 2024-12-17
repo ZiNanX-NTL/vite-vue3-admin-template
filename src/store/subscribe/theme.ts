d94d50143682d207b1b20b374f2cb2ed9887365c
ZiNanX_NTL <702668400@qq.com>
2024-12-17 11:15:08 +0800

    perf: 移除主题变量冗余设置
    - 删除了重复设置主题变量的代码行
    - 优化了主题切换逻辑，减少不必要的操作

--- src/store/subscribe/theme.ts
+++ src/store/subscribe/theme.ts
@@ -41,7 +41,6 @@ export default function subscribeThemeStore() {
         } else {
           removeDarkClass();
         }
-        addThemeCssVarsToHtml(theme.naiveThemeOverrides.common);
       },
       {
         immediate: true
